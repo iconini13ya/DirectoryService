886eee921fc1667d3dfde6742e46736a5a7f9fa4
Vladimir Yarantsev <gglobalink@gmail.com>
2026-01-31 18:31:26 +0300

    Обновил базовые модели

--- DirectoryService.Entities/Location.cs
+++ DirectoryService.Entities/Location.cs
@@ -1,79 +1,38 @@
-﻿namespace DirectoryService.Entities;
+﻿using static DirectoryService.Entities.ValueObjects;
+
+namespace DirectoryService.Entities;
 
 public class Location
 {
-	private List<Department> _departments = [];
-
-
 	public Location(
-		string name, 
+		Name name, 
 		string address,
-		string timeZone,
+        ValueObjects.TimeZone timeZone,
+		bool isActive,
 		DateTime createdAt,
-		DateTime updatedAt,
-		IEnumerable<Department> departments,
-        bool? isActive)
-    { 
+		DateTime updatedAt
+		)
+	{ 
 		Id = Guid.NewGuid();
 		Name = name;
 		Address = address;
 		TimeZone = timeZone;
-		IsActive = isActive ?? true;
+		IsActive = true;
 		CreatedAt = createdAt;
 		UpdatedAt = updatedAt;
-		_departments = GetDepartments(departments);
 	}
 
-    private List<Department> GetDepartments(IEnumerable<Department> departments)
-    {
-        if (departments == null || !departments.Any())
-        {
-            throw new ArgumentException("Property departments should contain at least one item");
-        }
-        return departments.ToList();
-    }
+	public Guid Id {  get; private set; }
 
-    public Guid Id {  get; private set; }
-
-	public string Name
-	{
-		get { return Name; }
-		private set
-		{
-			if (string.IsNullOrWhiteSpace(value) || value.Length > 150 || value.Length < 3)
-			{
-				throw new ArgumentException("Property name should not be empty, less than 3 and bigger than 150 symbols");
-			}
-
-			// Добавить логику обработки уникального значения
-			Name = value;
-		}
-	}
+	public Name Name { get; private set; }
 
 	public string Address {get; private set;}
 
-	public string TimeZone 
-	{
-		get { return TimeZone; } 
-		private set
-		{
-			try
-			{
-				TimeZoneInfo.FindSystemTimeZoneById(value);
-				TimeZone = value;
-			}
-			catch(Exception e)
-			{
-				throw new ArgumentException($"Wrong argument {value}, that trigger exception {e}");
-			}
-		}
-	}
+	public ValueObjects.TimeZone TimeZone { get; private set; }
 
 	public bool IsActive { get; set; }
 
 	public DateTime CreatedAt { get; private set; }
 
 	public DateTime UpdatedAt { get; private set; }
-
-	public IReadOnlyList<Department> Departments => _departments;
 }
--- DirectoryService.Entities/Position.cs
+++ DirectoryService.Entities/Position.cs
@@ -1,72 +1,35 @@
-﻿namespace DirectoryService.Entities;
+﻿using static DirectoryService.Entities.ValueObjects;
+
+namespace DirectoryService.Entities;
 
 public class Position
 {
-	private List<Department> _departments = [];
-
 	public Position(
-		string name,
+		Name name,
+		Description description,
+		bool isActive,
 		DateTime createdAt,
-		DateTime updatedAt,
-		string? description,
-		bool? isActive,
-		IEnumerable<Department> departments)
+		DateTime updatedAt
+		)
 	{
 		Id = Guid.NewGuid();
 		Name = name;
 		Description = description;
-		IsActive = isActive ?? true;
+		IsActive = true;
 		CreatedAt = createdAt;
 		UpdatedAt = updatedAt;
-		_departments = GetDepartments(departments);
-	}
-
-	private List<Department> GetDepartments(IEnumerable<Department> departments)
-	{
-		if (departments == null || !departments.Any())
-		{
-			throw new ArgumentException("Property departments should contain at least one item");
-		}
-		return departments.ToList();
 	}
 
 	public Guid Id { get; private set; }
 
-	public string Name
-	{
-		get { return Name; }
-		private set
-		{
-			if (string.IsNullOrWhiteSpace(value) || value.Length > 100 || value.Length < 3)
-			{
-				throw new ArgumentException("Property name should not be empty, less than 3 and bigger than 100 symbols");
-			}
+	public Name Name { get; private set; }
 
-			// Добавить логику обработки уникального значения
-			Name = value;
-		}
-	}
-
-	public string? Description
-	{
-		get { return Description; }
-		private set
-		{
-			if (value?.Length > 1000)
-			{
-				throw new ArgumentException("Property description should not be bigger than 1000 symbols");
-			}
-
-			Description = value;
-		}
-	}
+	public Description Description { get; private set; }
 
 	public bool IsActive { get; set; }
 
 	public DateTime CreatedAt { get; private set; }
 
 	public DateTime UpdatedAt { get; private set; }
-
-    public IReadOnlyList<Department> Departments => _departments;
 }
 
