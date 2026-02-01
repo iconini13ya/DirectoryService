74a0682d83dd0f05b979acf16da070b2ca220412
Vladimir Yarantsev <gglobalink@gmail.com>
2026-02-01 21:22:02 +0300

    Вынес константы в VO вверх. Поставил их в высоком регистре.

--- DirectoryService.Entities/ValueObjects/Identifier.cs
+++ DirectoryService.Entities/ValueObjects/Identifier.cs
@@ -4,17 +4,18 @@ namespace DirectoryService.Entities.ValueObjects;
 
 public record Identifier
 {
+    public const int MAXLENGTH = 30;
+    public const int MINLENGTH = 3;
+
     public Identifier(string identifier)
     {
-        if (string.IsNullOrWhiteSpace(identifier) || identifier.Length > 30 || identifier.Length < 3 || !Regex.IsMatch(identifier, @"^[a-zA-Z]+$"))
+        if (string.IsNullOrWhiteSpace(identifier) || identifier.Length > MAXLENGTH || identifier.Length < MINLENGTH || !Regex.IsMatch(identifier, @"^[a-zA-Z]+$"))
         {
-            throw new ArgumentException($"Property identifier should not : be empty, less than {MinLength} and bigger than {MaxLength} symbols and contains only latin alphabet");
+            throw new ArgumentException($"Property identifier should not : be empty, less than {MINLENGTH} and bigger than {MAXLENGTH} symbols and contains only latin alphabet");
         }
 
         Value = identifier;
     }
 
-    public const int MaxLength = 30;
-    public const int MinLength = 3;
     public string Value { get; }
 }
--- DirectoryService.Entities/ValueObjects/Name.cs
+++ DirectoryService.Entities/ValueObjects/Name.cs
@@ -2,17 +2,18 @@
 
 public record Name
 {
+    public const int MAXLENGTH = 150;
+    public const int MINLENGTH = 3;
+
     public Name(string name)
     {
-        if (string.IsNullOrWhiteSpace(name) || name.Length > MaxLength || name.Length < MinLength)
+        if (string.IsNullOrWhiteSpace(name) || name.Length > MAXLENGTH || name.Length < MINLENGTH)
         {
-            throw new ArgumentException($"Property Department name should not : be empty, less than {MinLength} and bigger than {MaxLength} symbols");
+            throw new ArgumentException($"Property Department name should not : be empty, less than {MINLENGTH} and bigger than {MAXLENGTH} symbols");
         }
 
         Value = name;
     }
 
-    public const int MaxLength = 150;
-    public const int MinLength = 3;
     public string Value { get; }
 }
