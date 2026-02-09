f222737644f5b54929b50d0e07ef99e34afba94a
Vladimir Yarantsev <gglobalink@gmail.com>
2026-02-09 22:16:46 +0300

    Добавил сам DB Context

---
+++ DirectoryService.Infrastructure.Postgres/DirectoryServiceDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace DirectoryService.Infrastructure.Postgres;
+
+public class DirectoryServiceDbContext : DbContext
+{
+    private readonly string _connectionString;
+
+    public DirectoryServiceDbContext(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DirectoryServiceDbContext).Assembly);
+    }
+    //var source = NpgsqlDataSource.Create()
+}
