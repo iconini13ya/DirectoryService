451352707c6ebe5b7045742647b370285d466556
Vladimir Yarantsev <gglobalink@gmail.com>
2026-03-16 21:30:20 +0300

    Паттерн репозитори. Пока сделал GET - null, чтобы можно былдо протестировать. Заклад на будущее.

---
+++ DirectoryService.Infrastructure.Postgres/Repositories/Location/ILocationRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DirectoryService.Infrastructure.Postgres.Repositories.Location;
+
+public interface ILocationRepository
+{
+    Task AddAsync(Entities.Location.Location location, CancellationToken cancellationToken);
+
+    Task<Guid?> GetLocationByNameAsync(Entities.ValueObjects.Name locationName, CancellationToken cancellationToken);
+
+    Task<Guid?> GetLocationByAddressAsync(Entities.ValueObjects.Address locationAddress, CancellationToken cancellationToken);
+}
---
+++ DirectoryService.Infrastructure.Postgres/Repositories/Location/LocationsRepository.cs
@@ -0,0 +1,30 @@
+﻿using DirectoryService.Entities.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.Postgres.Repositories.Location;
+
+public sealed class LocationsRepository : ILocationRepository
+{
+    private readonly DirectoryServiceDbContext _context;
+    public LocationsRepository(DirectoryServiceDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Entities.Location.Location location, CancellationToken cancellationToken)
+    {
+        await _context.Locations.AddAsync(location, cancellationToken);
+
+        _context.SaveChanges();
+    }
+
+    public async Task<Guid?> GetLocationByAddressAsync(Address locationAddress, CancellationToken cancellationToken)
+    {
+        return null;
+    }
+
+    public async Task<Guid?> GetLocationByNameAsync(Name locationName, CancellationToken cancellationToken)
+    {
+        return null;
+    }
+}
