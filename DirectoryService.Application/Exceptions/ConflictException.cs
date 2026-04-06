f147a56672398d85907c5783c42808a7d1dcd28b
Vladimir Yarantsev <gglobalink@gmail.com>
2026-04-06 21:59:03 +0300

    Добавил классы ошибок

---
+++ DirectoryService.Application/Exceptions/ConflictException.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using Shared;
+
+namespace DirectoryService.Application.Exceptions;
+
+public class ConflictException : Exception
+{
+    public ConflictException(Error[] errors)
+        : base(JsonSerializer.Serialize(errors))
+    {
+
+    }
+}
---
+++ DirectoryService.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using Shared;
+using System.Text.Json;
+
+namespace DirectoryService.Application.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException(Error[] errors)
+        : base(JsonSerializer.Serialize(errors))
+    {
+
+    }
+}
