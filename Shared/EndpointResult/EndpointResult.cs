a8f778c9c1a2392334b3c930bac58ba81402326d
Vladimir Yarantsev <gglobalink@gmail.com>
2026-04-13 21:36:42 +0300

    Добавил реализацию интерфейса IEndpointMetadataProvider

--- Shared/EndpointResult/EndpointResult.cs
+++ Shared/EndpointResult/EndpointResult.cs
@@ -1,9 +1,12 @@
 ﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Metadata;
+using System.Reflection;
 
 namespace Shared.EndpointResult;
 
-public sealed class EndpointResult<TValue> : Microsoft.AspNetCore.Http.IResult
+public sealed class EndpointResult<TValue> : Microsoft.AspNetCore.Http.IResult , IEndpointMetadataProvider
 {
     private readonly Microsoft.AspNetCore.Http.IResult _result;
 
@@ -12,6 +15,19 @@ public sealed class EndpointResult<TValue> : Microsoft.AspNetCore.Http.IResult
         _result = result.IsSuccess
             ? new SuccessResult<TValue>(result.Value)
             : new ErrorResult(result.Error);
+    }
+
+    public static void PopulateMetadata(MethodInfo method, EndpointBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(method);
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(200, typeof(Envelope<TValue>), ["application/json"]));
+
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(500, typeof(Envelope<TValue>), ["application/json"]));
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(400, typeof(Envelope<TValue>), ["application/json"]));
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(404, typeof(Envelope<TValue>), ["application/json"]));
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(409, typeof(Envelope<TValue>), ["application/json"]));
     }
 
     public Task ExecuteAsync(HttpContext httpContext)
