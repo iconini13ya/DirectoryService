f37e021851a7ea07be9dc6bba4ad2bc686cc0db6
Vladimir Yarantsev <gglobalink@gmail.com>
2026-04-13 21:19:44 +0300

    Добавил эндпоинт резалты

---
+++ Shared/EndpointResult/EndpointResult.cs
@@ -0,0 +1,23 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Http;
+
+namespace Shared.EndpointResult;
+
+public sealed class EndpointResult<TValue> : Microsoft.AspNetCore.Http.IResult
+{
+    private readonly Microsoft.AspNetCore.Http.IResult _result;
+
+    public EndpointResult(Result<TValue, Error[]> result)
+    {
+        _result = result.IsSuccess
+            ? new SuccessResult<TValue>(result.Value)
+            : new ErrorResult(result.Error);
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        return _result.ExecuteAsync(httpContext);
+    }
+
+    public static implicit operator EndpointResult<TValue>(Result<TValue, Error[]> result) => new(result);
+}
---
+++ Shared/EndpointResult/SuccessResult.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Shared.EndpointResult;
+
+public class SuccessResult<TValue> : IResult
+{
+    public readonly TValue _value;
+
+    public SuccessResult(TValue value)
+    {
+        _value = value;
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        var envelope = Envelope<TValue>.Ok(_value);
+
+        httpContext.Response.StatusCode = StatusCodes.Status200OK;
+        httpContext.Response.ContentType = "application/json";
+
+        return httpContext.Response.WriteAsJsonAsync(envelope);
+    }
+}
