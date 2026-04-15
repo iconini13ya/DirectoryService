9555ac31e0399090e829c4ee17a436c1672d546b
Vladimir Yarantsev <gglobalink@gmail.com>
2026-04-15 21:27:36 +0300

    Убрал ненужный Middleware, Убрал ошибки из контроллера и добавил их в .editorconfig

--- DirectoryService/Controllers/LocationsController.cs
+++ DirectoryService/Controllers/LocationsController.cs
@@ -7,11 +7,9 @@ namespace DirectoryService.Controllers;
 
 [ApiController]
 [Route("api/locations")]
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "<Pending>")]
 public class LocationsController : ControllerBase
 {
     [HttpPost]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>")]
     public async Task<EndpointResult<Guid>> Create(
         [FromServices] LocationsService locationService,
         [FromBody] CreateLocationDto dto,
